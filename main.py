fc3057b8b02718e202f42d044eb248b0327b7b5c
rawasary72-sys <rawasary72@gmail.com>
2026-04-08 13:30:00 +0300

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from kivymd.app import MDApp
-from kivymd.uix.label import MDLabel
-
-class CobraApp(MDApp):
-    def build(self):
-        return MDLabel(text="سڵاو ڕەوا، ئەمە بەرنامەکەتە", halign="center")
-
-if __name__ == "__main__":
-    CobraApp().run()
